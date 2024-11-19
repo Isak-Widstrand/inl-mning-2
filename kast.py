35e694d64d7912830f0e6b0cffd5515c1784f944
Isak-Widstrand <90836489+Isak-Widstrand@users.noreply.github.com>
2024-11-19 22:54:13 +0100

    Add files via upload

---
+++ kast.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+while True:
+    plt.figure(figsize=(5, 4))
+    plt.ion()
+    plt.xlabel("Längd (m)")
+    plt.ylabel("Höjd (m)")
+    plt.title("Kastparabel")
+    plt.grid(True)
+    grader = input("Ange grader: ")
+    grader = float(grader)
+    V0 = input("Ange utgångshastigheten: ")
+    V0 = float(V0)
+    g = 9.81
+    alfa = np.radians(grader)
+    airtime = 2 * V0 * np.sin(alfa) / g
+    tid = np.arange(0, airtime, 0.005)
+    x_distans = V0 * tid * np.cos(alfa)
+    y_distans = V0 * tid * np.sin(alfa) - g / 2 * tid ** 2
+
+    plt.plot(x_distans, y_distans)
+    plt.vlines (x=15, ymin=0, ymax=5)
+    plt.draw()
+    plt.show()
+
+    bra = input("Är du nöjd med resultatet? (ja/nej): ").strip().lower()
+    if bra == "ja":
+        break
+    else: 
+        print("ange nya värden: ")
